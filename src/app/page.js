f5d5f4f5a0b5e602e38498ac2d1a6bfe454d81c4
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-23 08:51:33 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
     <div className="ml-4 mr-4 mt-24">
-    <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-8">Hi, I'm NanotechPikachu!</h1>
+    <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-8">Hi, I&apos;m NanotechPikachu!</h1>
     </div>
     </>
   )
