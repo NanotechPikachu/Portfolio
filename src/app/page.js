74693761c45b8f3bcbd8714451350dadb68dc0ee
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-23 09:07:50 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ export default function Home() {
     <>
     <div className="ml-4 mr-4 mt-24">
     <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4">Hi, I&apos;m NanotechPikachu!</h1>
-    <p className="text-base dark:text-zinc-50 text-black md:text-lg text-center">A developer with passion!</p>
+    <p className="text-sm dark:text-zinc-50 text-black md:text-base text-center">A developer with passion!</p>
     </div>
     <div className="mt-6 mr-3 ml-3 border dark:border-emerald-600 border-emerald-400">
-    <p className="md:text-lg text-base dark:text-zinc-50 text-black mr-2 ml-2 mt-2 mb-2">
+    <p className="font-serif md:text-base text-sm dark:text-zinc-50 text-black mr-2 ml-2 mt-2 mb-2">
 I&apos;m a <b>Computer Science</b> student and an ardent fan of programming. A developer and an author by passion. Love to make webpages and many other things!
     </p>
     </div>
