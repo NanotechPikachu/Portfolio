ea12ad01bcea12c18498b385444548e96105b3f7
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 13:37:14 +0530

    Update PLKnown.jsx

--- src/components/PLKnown.jsx
+++ src/components/PLKnown.jsx
@@ -2,7 +2,7 @@ export default function PLKnown() {
   let p = ["NodeJS", "Starlight(Docs)", "Docus(Docs)", "NextJS", "Python", "ReactJS", "TailwindCSS", "HTML", "CSS", "Markdown", "DiscordJS", "AoiJS"];
   return (
     <>
-    <div className="mt-4 ml-2 mr-2 mb-2">
+    <div className="mt-4 ml-2 mr-2 mb-2 flex flex-wrap">
     {
     p.map((x) => (
       <div key={x} className="p-2 m-2 dark:bg-cyan-400 bg-cyan-500 text-center hover:p-3 font-bold text-black border border-amber-100">
