65129000866701cf86c9b70d0e6886a39031ed5a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 15:05:14 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,15 @@ import TechStack from '../components/TechStack.jsx';
 export default function Home() {
   return (
     <>
+    <div className="w-full h-full">
     <div className="ml-8 mr-8 mt-24 mb-20">
     <BasicInfo />
     <Socials />
     <TechStack />
+    </div>
+    <div className="bg-blue-500 bottom-0 text-center">
+    <p>hi</p>
+    </div>
     </div>
     </>
   )
