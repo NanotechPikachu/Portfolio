474e1335627a8e5c97f22eabab7326e990608c8e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 12:09:14 +0530

    BasicInfo

---
+++ src/components/BasicInfo.jsx
@@ -0,0 +1,13 @@
+export default BasicInfo() {
+  return (
+    <>
+    <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4">Hi, I&apos;m NanotechPikachu!</h1>
+    <p className="text-sm dark:text-zinc-50 text-black md:text-base text-center">A developer with passion!</p>
+    <div className="mt-6 mr-2 ml-2 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100 border">
+    <p className="font-serif md:text-base text-sm dark:text-zinc-50 text-black mr-3 ml-3 mt-2 mb-2">
+I&apos;m a <b>Computer Science</b> student and an ardent fan of programming. A developer and an author by passion. Love to make webpages and many other things!
+    </p>
+    </div>
+    </>
+  )
+}
