595b55d916c1b216cbd4e86a719125d65133c6e7
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 13:24:08 +0530

    Update PLKnown.jsx

--- src/components/PLKnown.jsx
+++ src/components/PLKnown.jsx
@@ -1,8 +1,15 @@
 export default function PLKnown() {
-  let p = [ "NodeJS", "Starlight(Docs)", "NextJS", "Python", "ReactJS", "TailwindCSS", "HTML", "CSS", "Markdown", "DiscordJS", "AoiJS" ];
+  let p = [ "NodeJS", "Starlight(Docs)", "Docus(Docs)", "NextJS", "Python", "ReactJS", "TailwindCSS", "HTML", "CSS", "Markdown", "DiscordJS", "AoiJS" ];
   return (
     <>
     <div className="mt-4 ml-2 mr-2 mb-2">
+    {
+    p.map((x) => {
+      <button className="p-2 m-2 dark:bg-cyan-400 bg-cyan-500 text-center hover:p-3 font-bold text-black border border-amber-100">
+      {x}
+      </button>
+    });
+    }
     </div>
     </>
   )
