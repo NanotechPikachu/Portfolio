b15388d930e5e05b013465fde03bf4b535af258a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 15:17:38 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,13 @@ import TechStack from '../components/TechStack.jsx';
 export default function Home() {
   return (
     <>
-    <div className="w-full h-full">
+    <div className="flex flex-col min-h-screen">
+    <div className="flex-grow">
     <div className="ml-8 mr-8 mt-24 mb-20">
     <BasicInfo />
     <Socials />
     <TechStack />
+    </div>
     </div>
     <div className="dark:bg-teal-100 bg-teal-200 bottom-0 text-center text-black text-xs md:text-sm font-mono">
     <p>Made with &hearts; and NextJS by <span className="font-bold italic">NanotechPikachu</span></p>
