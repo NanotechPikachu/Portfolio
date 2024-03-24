740048a3885cc73ef671df4059766ef83ed65d5c
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 13:33:00 +0530

    Update PLKnown.jsx

--- src/components/PLKnown.jsx
+++ src/components/PLKnown.jsx
@@ -4,11 +4,11 @@ export default function PLKnown() {
     <>
     <div className="mt-4 ml-2 mr-2 mb-2">
     {
-    p.map((x) => {
-      <button className="p-2 m-2 dark:bg-cyan-400 bg-cyan-500 text-center hover:p-3 font-bold text-black border border-amber-100">
+    p.map((x) => (
+      <div className="p-2 m-2 dark:bg-cyan-400 bg-cyan-500 text-center hover:p-3 font-bold text-black border border-amber-100">
       {x}
-      </button>
-    })
+      </div>
+    ))
     }
     </div>
     </>
