63eab18e1346371b0bbe76cfb3a41a2da8df2fdd
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 12:26:14 +0530

    Update Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -4,7 +4,7 @@ export default function Socials() {
   return (
     <>
     <div className="mr-2 ml-2 mt-6 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100">
-    <h2 className="text-center text-xl md:text-2xl text-black dark:text-white font-bold"><u>Socials</u></h2>
+    <h2 className="text-center text-xl md:text-2xl text-black dark:text-white font-bold mt-2"><u>Socials</u></h2>
     <p className="mt-2 dark:text-slate-50 text-black text-center font-mono md:text-base text-sm"><i>Contact me using the below methods...</i></p>
     <div className="mt-2 mb-2 flex items-center justify-center">
     <a href="https://github.com/NanotechPikachu">
