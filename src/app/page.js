cedcaf518e8972b4f1343bdf05f1fde60052a760
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 12:07:41 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ I&apos;m a <b>Computer Science</b> student and an ardent fan of programming. A d
     </p>
     </div>
     <div className="mr-2 ml-2 mt-6 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100">
-    <h2 className="text-center text-xl md:text-2xl text-black dark:text-white font-bold">Socials</h2>
+    <h2 className="text-center text-xl md:text-2xl text-black dark:text-white font-bold"><u>Socials</u></h2>
     <p className="mt-2 dark:text-slate-50 text-black text-center font-mono md:text-base text-sm"><i>Contact me using the below methods...</i></p>
     <div className="mt-2 mb-2 flex items-center justify-center">
     <a href="https://github.com/NanotechPikachu">
