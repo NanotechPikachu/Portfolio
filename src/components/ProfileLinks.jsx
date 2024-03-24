4594e1f352e13a683d1f158d2f547103e73645ca
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 10:13:48 +0530

    Profile Links

---
+++ src/components/ProfileLinks.jsx
@@ -0,0 +1,20 @@
+import { FaGithub, FaDiscord } from 'react-icons/fa';
+
+function Git() {
+  return (
+    <>
+    <FaGithub size="40px" color="#6ddcf2" />
+    </>
+  )
+}
+
+function Discord() {
+  return (
+    <>
+    <FaDiscord size="40px" color="#6ddcf2" />
+    </>
+  )
+}
+
+export { Git, Discord };
+
