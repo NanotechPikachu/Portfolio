1dc9cd0f19dbe3e3f9d9c24c0855f9398a9be9a5
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 12:33:33 +0530

    Update TechStack.jsx

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -4,7 +4,7 @@ export default function TechStack() {
     <div className="mr-2 ml-2 mt-6 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100">
     <div className="mt-2 mr-2 ml-2 mb-2">
     <h2 className="text-center dark:text-white text-black underline text-xl md:text-2xl"><b>Tech Stack</b></h2>
-    <p className="mt-2 text-center text-black dark:text-slate-50 text-sm md:text-base"><i>
+    <p className="mt-2 text-center text-black dark:text-slate-50 text-sm md:text-base font-mono"><i>
 I&apos;m basically a website developer and Discord bot maker focusing on JS.
     </i></p>
     </div>
