3633d339b42f1ee552e11e46039a532c936de2c2
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 13:43:10 +0530

    Update PLKnown.jsx

--- src/components/PLKnown.jsx
+++ src/components/PLKnown.jsx
@@ -5,7 +5,7 @@ export default function PLKnown() {
     <div className="mt-4 ml-2 mr-2 mb-2 flex flex-wrap">
     {
     p.map((x) => (
-      <div key={x} className="p-2 m-2 dark:bg-cyan-400 bg-cyan-500 text-center hover:text-[15px] md:hover:text-[18px] font-bold text-black text-small md:text-base rounded-lg border border-amber-100 text-sans">
+      <div key={x} className="p-2 m-2 dark:bg-cyan-400 bg-cyan-500 text-center hover:text-[15px] md:hover:text-[17px] font-bold text-black text-sm md:text-base rounded-lg border border-amber-100 text-sans">
       {x}
       </div>
     ))
