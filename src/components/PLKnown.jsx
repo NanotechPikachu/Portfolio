a768b7881fb2c4ac127c7845cd25491953a91fd0
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-24 12:43:37 +0530

    Update PLKnown.jsx

--- src/components/PLKnown.jsx
+++ src/components/PLKnown.jsx
@@ -1,9 +1,9 @@
 export default function PLKnown() {
-  const p = [ "NodeJS", "Starlight(Docs)", "NextJS", "Python", "ReactJS", "TailwindCSS", "HTML", "CSS", "Markdown", "DiscordJS", "AoiJS" ];
+  let p = [ "NodeJS", "Starlight(Docs)", "NextJS", "Python", "ReactJS", "TailwindCSS", "HTML", "CSS", "Markdown", "DiscordJS", "AoiJS" ];
   return (
     <>
     <div className="mt-4 ml-2 mr-2 mb-2">
-    <div>
+    </div>
     </>
   )
 }
