afaf4662e5de70dfb95f2461da745ae26c4a7750
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-25 07:33:19 +0530

    Update BasicInfo.jsx

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -4,10 +4,8 @@ export default function BasicInfo() {
     <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4">Hi, I&apos;m NanotechPikachu!</h1>
     <p className="font-mono text-sm dark:text-zinc-50 text-black md:text-base text-center"><i>A developer with passion!</i></p>
     <div className="mt-6 mr-2 ml-2 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100 border">
-    <p className="md:text-base text-sm dark:text-zinc-50 text-black mr-3 ml-3 mt-2 mb-2">
-I haven&apos;t worked on any officially recognized project but have been working on different simple projects of my own and also helped others in whatever way I could. 
-    <br /><br />
-As such, I don&apos;t have any official experience nor any certificates to prove my skills. But, I can assure that whatever work I do, <i>I will put my whole heart into it!</i>
+    <p className="font-sans md:text-base text-sm dark:text-zinc-50 text-black mr-3 ml-3 mt-2 mb-2">
+I'm a <b>Computer Science</b> student and an ardent fan of programming. A developer and an author by passion. Love to make webpages and many other things!
     </p>
     </div>
     </>
