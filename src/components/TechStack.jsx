691f7cf43d87b5dbfb045bc9bd283eba7312600e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-25 19:52:26 +0530

    Update TechStack.jsx

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,7 +3,7 @@ import PLKnown from './PLKnown.jsx';
 export default function TechStack() {
   return (
     <>
-    <div className="mr-2 ml-2 mt-6 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100">
+    <div className="mr-2 ml-2 mt-6 border dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
     <div className="mt-2 mr-2 ml-2 mb-2">
     <h2 className="text-center dark:text-white text-black underline text-xl md:text-2xl"><b>Tech Stack</b></h2>
     <p className="mt-2 text-center text-black dark:text-slate-50 text-sm md:text-base font-mono"><i>
