2f32643f5671a06f64042b37f371417439ac6ab4
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-25 19:51:39 +0530

    Update Exp.jsx

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -1,7 +1,7 @@
 export default function Exp() {
   return (
     <>
-    <div className="mr-2 ml-2 mt-6 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100">
+    <div className="mr-2 ml-2 mt-6 border dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
     <h2 className="text-center text-xl md:text-2xl text-black dark:text-white font-bold mt-2"><u>Experience</u></h2>
     <p className="ml-2 mr-2 mt-2 mb-2 dark:text-slate-50 text-black md:text-center text-left font-sans md:text-base text-sm">
 I haven&apos;t worked on any officially recognized project but have been working on different simple projects of my own and also helped others in whatever way I could.
