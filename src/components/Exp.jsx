555c9fad232b49c976a25403549d9e06e6720843
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-25 07:34:20 +0530

    Update Exp.jsx

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -3,7 +3,11 @@ export default function Exp() {
     <>
     <div className="mr-2 ml-2 mt-6 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100">
     <h2 className="text-center text-xl md:text-2xl text-black dark:text-white font-bold mt-2"><u>Experience</u></h2>
-    <p className="mt-2 dark:text-slate-50 text-black text-center font-mono md:text-base text-sm"><i>Contact me using the below methods...</i></p>
+    <p className="mt-2 dark:text-slate-50 text-zinc-50 text-center font-mono md:text-base text-sm">
+I haven't worked on any officially recognized project but have been working on different simple projects of my own and also helped others in whatever way I could.
+<br /> <br />
+As such, I don't have any official experience nor any certificates to prove my skills. But, I can assure that whatever work I do, I will put my whole heart into it!
+    </p>
     </div>
     </>
   )
