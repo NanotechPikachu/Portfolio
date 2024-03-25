b820d5db93caac86c2742bc1bb7d1b80fd1ec370
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-25 20:28:58 +0530

    Update Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -3,10 +3,10 @@ import { Git, Discord } from './ProfileLinks.jsx';
 export default function Socials() {
   return (
     <>
-    <div className="mr-2 ml-2 mt-6 border dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
+    <div className="mr-6 ml-6 mt-6 border dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
     <h2 className="text-center text-xl md:text-2xl text-black dark:text-white font-bold mt-2"><u>Socials</u></h2>
     <p className="mt-2 dark:text-slate-50 text-black text-center font-mono md:text-base text-sm"><i>Contact me using the below methods...</i></p>
-    <div className="mt-2 mb-2 flex items-center justify-center">
+    <div className="mt-2 mb-6 flex items-center justify-center">
     <a href="https://github.com/NanotechPikachu">
     <button className="m-1 p-2 border dark:border-teal-500 border-teal-400">
     <Git />
