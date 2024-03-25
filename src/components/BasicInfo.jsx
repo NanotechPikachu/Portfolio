6fbf93e6235e85842e8cb1eaff4ce7d95c964243
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-25 07:34:53 +0530

    Update BasicInfo.jsx

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -5,7 +5,7 @@ export default function BasicInfo() {
     <p className="font-mono text-sm dark:text-zinc-50 text-black md:text-base text-center"><i>A developer with passion!</i></p>
     <div className="mt-6 mr-2 ml-2 border dark:border-emerald-600 border-emerald-400 dark:bg-[#2E3532] bg-slate-100 border">
     <p className="font-sans md:text-base text-sm dark:text-zinc-50 text-black mr-3 ml-3 mt-2 mb-2">
-I'm a <b>Computer Science</b> student and an ardent fan of programming. A developer and an author by passion. Love to make webpages and many other things!
+I&apos;m a <b>Computer Science</b> student and an ardent fan of programming. A developer and an author by passion. Love to make webpages and many other things!
     </p>
     </div>
     </>
