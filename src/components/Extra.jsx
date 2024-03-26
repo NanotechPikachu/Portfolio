9e814706bf4ea9e96c13bd6a3c22c1ab219d1b91
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-26 18:20:28 +0530

    Update Extra.jsx

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -5,6 +5,6 @@ export default function Extra() {
     <>
     <div className="mr-6 ml-6 mt-6 border-2 rounded-lg dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
     </div>
-    <>
+    </>
   )
 }
