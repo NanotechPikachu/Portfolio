1c1826f9e09680cb79212fc706a32984bc589876
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-26 18:17:27 +0530

    Extra div

---
+++ src/components/Extra.jsx
@@ -0,0 +1,10 @@
+import Callout from './Callout.jsx';
+
+export default function Extra() {
+  return (
+    <>
+    <div className="mr-6 ml-6 mt-6 border-2 rounded-lg dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
+    </div>
+    <>
+  )
+}
