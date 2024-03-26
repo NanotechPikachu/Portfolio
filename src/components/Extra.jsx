f80ca103aa339782920f89286e982c2765364390
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-26 18:22:37 +0530

    Update Extra.jsx

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -4,6 +4,9 @@ export default function Extra() {
   return (
     <>
     <div className="mr-6 ml-6 mt-6 border-2 rounded-lg dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
+    <Callout summary="Feel interested in knowing my hobbies? Click here!">
+    <p>Working....</p>
+    </Callout>
     </div>
     </>
   )
