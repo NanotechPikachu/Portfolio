99dd4e03912d33f058d125b690b60d0d8741e0ae
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-27 17:08:38 +0530

    Update ExtraInfo.jsx

--- src/components/ExtraInfo.jsx
+++ src/components/ExtraInfo.jsx
@@ -3,9 +3,9 @@ export function Hobbies() {
 
   return (
     <div className="mt-4 mb-2 border-t-2 border-teal-200 dark:border-teal-100 dark:text-slate-50 text-black text-sm md:text-base">
-    <h2 className="font-bold underline text-lg md:text-xl mb-3">Hobbies</h2>
+    <h2 className="font-bold underline text-lg md:text-xl mt-3 mb-3">Hobbies</h2>
     My hobbies are
-    <ul className="list-disc font-medium">
+    <ul className="mt-2 ml-6 mr-6 list-disc font-medium">
     {
     h.map((x) => (
       <li key={x}>{x}</li>
