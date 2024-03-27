8ca09e3932a5bc5d5e6afaf955a1be8839a34d36
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-27 07:07:21 +0530

    Update Callout.jsx

--- src/components/Callout.jsx
+++ src/components/Callout.jsx
@@ -12,7 +12,7 @@ export default function Callout({ summary, children }) {
   return (
     <div>
     <div className="cursor-pointer" onClick={handleCallout}>
-    <p className="text-sm md:text-base font-bold text-black dark:text-slate-50 ml-6 mr-6 mt-2 mb-2">{summary}</p>
+    <p className="text-sm md:text-base font-bold text-black dark:text-slate-50 ml-6 mr-6 mt-2 mb-2">{summary}&nbsp;&nbsp;&nbsp;&nbsp;&#x25B2;</p>
     </div>
     {isOpen && <div className="text-sm md:text-base text-black dark:text-slate-50 ml-6 mr-6 mb-6 mt-6">{children}</div>}
     </div>
