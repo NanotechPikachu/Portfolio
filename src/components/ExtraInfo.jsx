b295463562db560adc80100198ec677eb15bc7c6
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-27 20:01:52 +0530

    Update ExtraInfo.jsx

--- src/components/ExtraInfo.jsx
+++ src/components/ExtraInfo.jsx
@@ -22,14 +22,14 @@ function Fav() {
     <h2 className="font-bold underline text-lg md:text-xl mt-3 mb-3">My Favorites</h2>
     My favorites of the respective category
     <ul className="mt-2 ml-6 mr-6 list-disc font-medium">
-    <li key="anime"><span className="underline">Anime: </span>Sword Art Online</li>
-    <li key="webnovel"><span className="underline">WebNovel: </span>Nanomancer Reborn</li>  
-    <li key="lightNovel"><span className="underline">LightNovel: </span>Rascal Does Not Dream</li> 
-    <li key="manga"><span className="underline">Manga: </span>The Eminence In Shadow</li>    
-    <li key="manhwa"><span className="underline">Manhwa: </span>Solo Leveling</li>    
-    <li key="manhua"><span className="underline">Manhua: </span>Tales Of Demons And Gods</li>    
-    <li key="artist"><span className="underline">Music Artist: </span>NEFFEX</li>    
-    <li key="animeSong"><span className="underline">Anime Song: </span>Idol (Oshi No Ko)</li>
+    <li key="anime"><span className="underline">Anime</span>: Sword Art Online</li>
+    <li key="webnovel"><span className="underline">WebNovel</span>: Nanomancer Reborn</li>  
+    <li key="lightNovel"><span className="underline">LightNovel</span>: Rascal Does Not Dream</li> 
+    <li key="manga"><span className="underline">Manga</span>: The Eminence In Shadow</li>    
+    <li key="manhwa"><span className="underline">Manhwa</span>: Solo Leveling</li>    
+    <li key="manhua"><span className="underline">Manhua</span>: Tales Of Demons And Gods</li>    
+    <li key="artist"><span className="underline">Music Artist</span>: NEFFEX</li>    
+    <li key="animeSong"><span className="underline">Anime Song</span>: Idol (Oshi No Ko)</li>
     </ul>
     </div>
   )
