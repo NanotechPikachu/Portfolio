f20742439c451980e11027e2eb9bb7e30fc39052
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-27 17:04:09 +0530

    Update ExtraInfo.jsx

--- src/components/ExtraInfo.jsx
+++ src/components/ExtraInfo.jsx
@@ -1,7 +1,17 @@
 export default function Hobbies() {
+  let h = [ "Watch Anime", "Read Novels", "Read Mangas, Manhwas, Manhuas", "Programming", "Listening to Music", "And much more..." ];
+
   return (
     <div className="mt-4 mb-2 border-t-2 border-teal-200 dark:border-teal-100 dark:text-slate-50 text-black text-sm md:text-base">
-    
+    <h2 className="font-bold underline text-lg md:text-xl mb-3">Hobbies</h2>
+    My hobbies are
+    <ul className="list-disc font-medium">
+    {
+    h.map((x) => (
+      <li key={x}>{x}</li>
+    ))
+    }
+    </ul>
     </div>
   )
 }
