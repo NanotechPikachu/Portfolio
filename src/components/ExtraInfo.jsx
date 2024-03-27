bd40a9e1727ae764ccae7501bf547f2321add9cc
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-27 20:05:05 +0530

    Update ExtraInfo.jsx

--- src/components/ExtraInfo.jsx
+++ src/components/ExtraInfo.jsx
@@ -35,4 +35,11 @@ function Fav() {
   )
 }
 
+// Randomizer JS Function 
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
 export { Hobbies, Fav }
