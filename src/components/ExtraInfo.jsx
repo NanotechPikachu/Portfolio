fd0c5cead887655829f1afa42162036e22fcd4d2
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-27 16:56:01 +0530

    ExtraInfo!

---
+++ src/components/ExtraInfo.jsx
@@ -0,0 +1,7 @@
+export default function Hobbies() {
+  return (
+    <div className="mt-4 mb-2 border-t-2 border-teal-200 dark:border-teal-100 dark:text-slate-50 text-black text-sm md:text-base">
+    
+    </div>
+  )
+}
