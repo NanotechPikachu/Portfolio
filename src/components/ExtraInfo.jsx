af38c019c3aac4f5a5ffc5b330648f9a6b6cbd40
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-28 12:05:49 +0530

    Update ExtraInfo.jsx

--- src/components/ExtraInfo.jsx
+++ src/components/ExtraInfo.jsx
@@ -17,14 +17,6 @@ function Hobbies() {
 }
 
 function Fav() {
-  let q = [ 
-"It’s impossible to work hard for something you don’t enjoy. ~ Silica",
-"Everybody can fight. It’s just a choice of whether you should. ~ Kirito",
-"Life isn’t just doing things for yourself. It’s possible to live in such a way that other people’s happiness, makes you happy too. ~ Asuna",
-"A person is very strong when he seeks to protect something. ~ Kayaba",
-"I\'d rather trust and regret, than doubt and regret. ~ Kirito"
-];
-
   return (
     <div className="mt-4 mb-2 border-t-2 border-teal-200 dark:border-teal-100 dark:text-slate-50 text-black text-sm md:text-base">
     <h2 className="font-bold underline text-lg md:text-xl mt-3 mb-3">My Favorites</h2>
@@ -43,11 +35,5 @@ function Fav() {
   )
 }
 
-// Randomizer JS Function 
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
 
 export { Hobbies, Fav }
