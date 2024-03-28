d634cea9d5fd1ac3dc41f6ba528ff4f6d05b96cf
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-28 12:05:12 +0530

    Randomizer

---
+++ src/components/Random.jsx
@@ -0,0 +1,11 @@
+/* 
+  ------- SUPPORT-FUNC -------
+These functions provide support to other functions.
+*/
+
+// Randomizer JS Function 
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
