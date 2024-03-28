d242725b8fca12ac64004979aeb9e1892c222dc1
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-28 12:20:53 +0530

    Update Random.jsx

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -19,10 +19,11 @@ function Quotes() {
 "I\'d rather trust and regret, than doubt and regret. ~ Kirito"
   ];
   let r = getRandom(0,5);
+  console.log(r);
   
   return (
     <>
-    <span className="dark:text-slate-50 text-black text-sm md:text-md hover:text-teal-100">{q[getRandom(0,5)]}</span>
+    <span className="dark:text-slate-50 text-black text-sm md:text-md hover:text-teal-100">{q[r]}</span>
     </>
   )
 }
