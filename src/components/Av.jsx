349a63c46ee239fdee191d1b61d5d155099b8b58
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-29 10:19:13 +0530

    Update Av.jsx

--- src/components/Av.jsx
+++ src/components/Av.jsx
@@ -1,3 +1,6 @@
+import avatar from '../app/Avatar.jpg';
+import Image from 'next/image';
+
 export default function Av() {
   return (
     <div className="ml-6 mr-6 flex flex-col md:flex-row md:justify-between items-center">
@@ -5,7 +8,11 @@ export default function Av() {
     <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4">Hi, I&apos;m NanotechPikachu!</h1>
     <p className="font-mono text-sm dark:text-zinc-50 text-black md:text-base text-center"><i>A developer with passion!</i></p>
     </div>
-    <h2 className="text-center">Hi</h2>
+    <div className="h-64 w-64 relative overflow-hidden rounded-full">
+    <Image
+      src={avatar} alt="Avatar of NanotechPikachu" layout="fill" objectFit="cover" className="rounded-full"
+    />
+    </div>
     </div>
   )
 }
