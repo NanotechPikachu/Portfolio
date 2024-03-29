3e15a84820f456f31c961c25d83dc7f0bb490932
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-29 10:01:34 +0530

    Update Av.jsx

--- src/components/Av.jsx
+++ src/components/Av.jsx
@@ -1,7 +1,10 @@
 export default function Av() {
   return (
     <div className="flex flex-col md:flex-row">
+    <div>
     <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4">Hi, I&apos;m NanotechPikachu!</h1>
+    <p className="font-mono text-sm dark:text-zinc-50 text-black md:text-base text-center"><i>A developer with passion!</i></p>
+    </div>
     <h2 className="text-center">Hi</h2>
     </div>
   )
