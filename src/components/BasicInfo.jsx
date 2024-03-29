35ab96fe051fa1e8e22a97c16645cb769bf2f209
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-29 10:01:08 +0530

    Update BasicInfo.jsx

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -3,8 +3,9 @@ export default function BasicInfo() {
     <>
 {/*
     <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4">Hi, I&apos;m NanotechPikachu!</h1>
-*/}
+
     <p className="font-mono text-sm dark:text-zinc-50 text-black md:text-base text-center"><i>A developer with passion!</i></p>
+*/}
     <div className="mt-6 mr-6 ml-6 border-2 rounded-lg dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
     <p className="font-sans md:text-center md:text-base text-sm dark:text-zinc-50 text-black mr-6 ml-6 mt-6 mb-6">
 I&apos;m a <b>Computer Science</b> student and an ardent fan of programming. A developer and an author by passion. Love to make webpages and many other things!
