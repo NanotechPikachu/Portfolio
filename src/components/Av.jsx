62dd06ad5df8d456ccc6616c3d961e8d02bac83f
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-29 10:21:47 +0530

    Update Av.jsx

--- src/components/Av.jsx
+++ src/components/Av.jsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 
 export default function Av() {
   return (
-    <div className="ml-6 mr-6 flex flex-col md:flex-row md:justify-between items-center">
+    <div className="ml-6 mr-6 flex flex-col-reverse md:flex-row md:justify-between items-center">
     <div>
-    <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4">Hi, I&apos;m NanotechPikachu!</h1>
-    <p className="font-mono text-sm dark:text-zinc-50 text-black md:text-base text-center"><i>A developer with passion!</i></p>
+    <h1 className="font-bold text-center text-2xl md:text-3xl dark:text-white text-black mb-4 mt-4 md:mt-0 md:ml-4">Hi, I&apos;m NanotechPikachu!</h1>
+    <p className="font-mono text-sm dark:text-zinc-50 text-black md:text-base text-center md:ml-4"><i>A developer with passion!</i></p>
     </div>
     <div className="h-64 w-64 relative overflow-hidden rounded-full">
     <Image
