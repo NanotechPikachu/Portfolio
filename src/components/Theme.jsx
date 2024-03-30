d8d5c44229645363abba5f2cc935a4ac15fb955b
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-30 11:19:33 +0530

    Update Theme.jsx

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -10,7 +10,7 @@ export default function Theme() {
 
   return (
     <div className="flex justify-end">
-    <select value={theme} onChange={e => setTheme(e.target.value)} data-test-id="theme-selector">
+    <select value={theme} onChange={e => setTheme(e.target.value)} data-test-id="theme-selector" className="text-sm border dark:border-teal-100 border-teal-200">
     <option value="system">System</option>
     {mounted && (
     <>
