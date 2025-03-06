6d0bac994909228cbf951094c1911e63df9392f8
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2025-03-06 09:18:38 +0530

    Update page.jsx

--- src/app/profiles/page.jsx
+++ src/app/profiles/page.jsx
@@ -1,7 +1,14 @@
 export default function Profiles() {
   return (
     <>
-<p>Hi</p>
+    <div className="flex flex-col min-h-screen">
+    <div className="flex-grow">
+    <Theme />
+    </div>
+    <div className="dark:bg-teal-100 bg-teal-200 bottom-0 text-center text-black text-xs md:text-sm font-mono">
+    <p className="m-4 md:m-6">Made with &hearts; and NextJS by <span className="font-bold italic">NanotechPikachu</span></p>
+    </div>
+    </div>
     </>
   )
 }
