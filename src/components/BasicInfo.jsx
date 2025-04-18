092b93e64e958752d1c8a50074dc788f505e4477
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2025-04-18 08:18:32 +0530

    Remove something

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -8,7 +8,7 @@ export default function BasicInfo() {
 */}
     <div className="mt-6 mr-6 ml-6 border-2 rounded-lg dark:border-teal-100 border-teal-200 dark:bg-[#2E3532] bg-slate-100">
     <p className="font-sans md:text-center md:text-base text-sm dark:text-zinc-50 text-black mr-6 ml-6 mt-6 mb-6">
-I&apos;m a <b>Computer Science</b> student and an ardent fan of programming. A developer and an author by passion. Love to make webpages and many other things!
+I&apos;m a <b>Computer Science</b> student and an ardent fan of programming. A developer by passion. Love to make webpages and many other things!
     </p>
     </div>
     </>
